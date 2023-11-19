be9a09fbf4baaf065c96a47b1067c69a1b6660b0
Aditi Dubey <aditidubey@ufl.edu>
2023-11-19 14:53:29 -0500

    added changes to counter unit test file

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,40 @@
 // import necessary react testing library helpers here
 // import the Counter component here
 
-beforeEach(() => {
-  // Render the Counter component here
-})
+import { render, screen } from "@testing-library/react";
+import Counter from '../components/Counter';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
+let counterComponent;
+
+beforeEach(() => {
+   counterComponent = render(<Counter />);
+})
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const messageForCounter = screen.getAllByText('Counter');
+  expect(messageForCounter[0]).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+   render(<Counter />);
+   const initial_value = screen.getAllByTestId('count');
+   expect(initial_value[0].textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const add = screen.getAllByText('+');
+  const value = screen.getAllByTestId('count');
+  userEvent.click(add[0]);
+  expect(value[0].textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const sub = screen.getAllByText('-');
+  const value = screen.getAllByTestId('count');
+  userEvent.click(sub[0]);
+  expect(value[0].textContent).toBe('-1');
 });
